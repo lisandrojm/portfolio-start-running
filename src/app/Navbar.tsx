f12036466d65309a5c0a4c430a1be5caa437ba03
lisandrojm <lisandrojm@gmail.com>
2023-12-17 11:05:58 -0300

    refactoring

---
+++ src/app/Navbar.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <nav className="m-3">
+      <div className="border rounded p-3 my-4 flex justify-between items-end">
+        <h1 className="m-1 tertiary__font italic text-3xl">Navbar</h1>
+        <ul className="flex">
+          <li className="m-1 pb-1">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="m-1">
+            <Link href="/form">Form</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      {/*       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
@@ -47,9 +47,12 @@ export default function Home() {
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
         </a>
-      </div>
+      </div> */}
       {/* Fonts test */}
-      <div className="border rounded p-3 my-4">
+      <div className="border rounded p-3 my-1">
+        <h1 className="tertiary__font italic text-3xl">Home</h1>
+      </div>
+      <div className="border rounded p-3 my-1">
         <h1 className="tertiary__font italic text-3xl">Fonts</h1>
         <p className={`primary__font`}>Roboto Mono</p>
         <p className={`secondary__font`}>Roboto Flex</p>
@@ -57,18 +60,6 @@ export default function Home() {
         <p className={`tertiary__font font-bold`}>Roboto Serif bold</p>
         <p className={`tertiary__font italic`}>Roboto Serif italic</p>
         <p className={`tertiary__font italic font-bold uppercase`}>Roboto Serif italic bold uppercase</p>
-      </div>
-      {/* Links test */}
-      <div className="border rounded p-3 my-4">
-        <h1 className="tertiary__font italic text-3xl">Links</h1>
-        <ul className="flex flex-col">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/form">Form</Link>
-          </li>
-        </ul>
       </div>
     </main>
   );
