577c47b902828ab4e928f93da6b696386c7c061c
lisandrojm <lisandrojm@gmail.com>
2023-12-17 18:53:51 -0300

    not-found.tsx-folder-structure

--- src/app/(routes)/not-found.tsx
+++ src/app/(routes)/not-found.tsx
@@ -3,10 +3,12 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div>
+    <div className="container mx-auto px-10">
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+      <Link href="/">
+        Return <span className="underline">Home</span>
+      </Link>
     </div>
   );
 }
--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -6,13 +6,17 @@ export default function Home() {
   return (
     <nav className="m-3">
       <div className="border rounded p-3 my-4 flex justify-between items-end">
-        <h1 className="m-1 tertiary__font italic text-3xl">Navbars</h1>
+        <h1 className="m-1 tertiary__font italic text-3xl">Navbar</h1>
         <ul className="flex">
           <li className="m-1 pb-1">
-            <Link href="/">Home</Link>
+            <Link className="underline" href="/">
+              Home
+            </Link>
           </li>
           <li className="m-1">
-            <Link href="/form">Form</Link>
+            <Link className="underline" href="/form">
+              Form
+            </Link>
           </li>
         </ul>
       </div>
