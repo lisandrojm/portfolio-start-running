cf23fc93bd6b2a9b07d994c33099ecbb8742f383
lisandrojm <lisandrojm@gmail.com>
2023-12-20 07:47:10 -0300

    folders-architecture

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,10 +1,11 @@
 /* src/app/_components/Footer.tsx */
 
 import Link from 'next/link';
+import styles from '@/_styles/_components/Footer.module.css';
 
 export default function Footer() {
   return (
-    <footer className="m-3">
+    <footer className={styles.footer}>
       <div className="border rounded p-3 my-4 flex justify-between items-end">
         <h1 className="m-1 tertiary__font italic text-3xl">Footer</h1>
         <ul className="flex">
--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -1,10 +1,11 @@
 /* src/app/_components/Navbar.tsx */
 
 import Link from 'next/link';
+import styles from '@/_styles/_components/Navbar.module.css';
 
 export default function Navbar() {
   return (
-    <nav className="m-3">
+    <nav className={`${styles.navbar} m-3`}>
       <div className="border rounded p-3 my-4 flex justify-between items-end">
         <h1 className="m-1 tertiary__font italic text-3xl">Navbar</h1>
         <ul className="flex">
