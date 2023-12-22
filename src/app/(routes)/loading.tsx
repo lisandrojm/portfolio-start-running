e93205f347f57d10670d8bb11f868d02ee9b35f6
lisandrojm <lisandrojm@gmail.com>
2023-12-22 09:38:23 -0300

    refactoring

--- src/app/(routes)/loading.tsx
+++ src/app/(routes)/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
   return (
     <div className="flex flex-col items-center justify-between">
       <div className="border rounded p-3 my-1 mb-3">
-        <h1 className="tertiary__font italic text-3xl">Loading...</h1>
+        <h1 className="text-3xl">Loading...</h1>
       </div>
     </div>
   );
