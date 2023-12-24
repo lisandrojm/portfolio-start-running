391cc582d3287c23b8d08a268ae75f25715a9642
lisandrojm <lisandrojm@gmail.com>
2023-12-24 00:18:26 -0300

    refactoring

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -17,7 +17,7 @@ export default function Navbar() {
           </li>
           <li className="m-1 mr-4 flex items-center">
             <Link className="underline" href="/form">
-              For
+              Form
             </Link>
           </li>
           <ThemeToggle />
