428d9342cfd534723603f6223ea8d2210cde6642
lisandrojm <lisandrojm@gmail.com>
2023-12-24 16:49:35 -0300

    heroicons

--- src/app/_components/ThemeSwitcher.tsx
+++ src/app/_components/ThemeSwitcher.tsx
@@ -1,6 +1,8 @@
 // src/app/_components/ThemeSwitcher.tsx
 'use client';
+
 import { Button } from '@/_components/_ui';
+import { MoonIcon, SunIcon } from '@heroicons/react/solid'; // Import MoonIcon and SunIcon from Heroicons
 
 import { useState, useEffect } from 'react';
 
@@ -20,5 +22,10 @@ export default function ThemeSwitcher() {
     setTheme(userPrefersDark ? 'dark' : 'light');
   }, []); // Solo se ejecuta una vez al montar el componente
 
-  return <Button onClick={toggleTheme}>Switch to {theme === 'dark' ? 'light' : 'dark'} mode</Button>;
+  return (
+    <Button onClick={toggleTheme}>
+      {/* Use the SunIcon and MoonIcon based on the current theme */}
+      {theme === 'dark' ? <MoonIcon className="w-6 h-6" /> : <SunIcon className="w-6 h-6" />}
+    </Button>
+  );
 }
